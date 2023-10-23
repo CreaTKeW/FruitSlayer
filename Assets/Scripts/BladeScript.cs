079b858727e49917339a6bf5b2c70b835f013309
Damian Ptaszkiewicz <kewinptaszkiewicz@gmail.com>
2023-10-23 22:19:37 +0200

    Fix
    Fixed all time blade collision detection with other objects

--- Assets/Scripts/BladeScript.cs
+++ Assets/Scripts/BladeScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class BladeScript : MonoBehaviour
 {
+    public float minVelocity = 0.1f;
+
     private Rigidbody2D rb;
+    private Vector3 lastMousePosition;
+    private Vector3 mouseVelocity;
+
+    private Collider2D collision;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collision = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        collision.enabled = IsMouseMoving();
         BladeAtMouse();
     }
 
@@ -22,5 +30,17 @@ public class BladeScript : MonoBehaviour
         var mousePosition = Input.mousePosition;
         mousePosition.z = 10; // distanced 10 units from z default value
         rb.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private bool IsMouseMoving()
+    {
+        Vector3 currentMousePosition = transform.position;
+        float travel = (lastMousePosition - currentMousePosition).magnitude;
+        lastMousePosition = currentMousePosition;
+
+        if(travel > minVelocity)
+        {
+            return true;
+        } else return false;
     }
 }
